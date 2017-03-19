fd57777e6b86d169e9e53777492d6ee9b9e71140
othman853 <anuar.yasser@gmail.com>
2017-03-19 17:17:54 -0300

    Parsing dates

--- event-parser.js
+++ event-parser.js
@@ -1,16 +1,26 @@
 require('value-box/path')(__dirname, ['/values']);
 const moment = require('moment');
 
-const { responseStub } = require('value-box');
+const { longStub, shortStub } = require('value-box');
 
-responseStub
+const date = wrapper => wrapper.date || wrapper.dateTime;
+
+shortStub
   .items
-  .filter(e => e.summary)
+  .filter(e => e.start)
+  .map( e => ({
+    name: e.summary,
+    start: moment(date(e.start), moment.ISO_8601),
+    end: moment(date(e.end), moment.ISO_8601)
+  }))
   .forEach(event => {
 
-    console.log(event.summary);
+    console.log(event.name + Array(50 - event.name.length).join(' ') + event.start.format('DD/MM/YYYY'));
+    console.log('-----------------------------------------------------');
+    // console.log('   ' + event.start.format('DD/MM/YYYY') + ' ~ ' + event.end.format('DD/MM/YYYY'));
+    // console.log('\n');
 
-    const startTime = event.start ? event.start.date || event.start.dateTime || '' : '';
-    console.log(startTime);
+    // const startTime = event.start ? event.start.date || event.start.dateTime || '' : '';
+    // console.log(startTime);
 
   });
