9a3faa260eb2da9e1a589ed42398cff28e99b2b2
othman853 <anuar.yasser@gmail.com>
2017-03-19 16:16:32 -0300

    Get calendar title

--- index.js
+++ index.js
@@ -5,36 +5,16 @@ const calendar = require('googleapis').calendar('v3');
 const authorize = require('./auth');
 const chalk = require('chalk');
 
-const datetime = str => {
-  const a = (str || '').split('T');
-  const date = a[0] || '';
-  const time = ((a[1] || '').substring[0,8]) || 'ALL_DAY';
-  return `${date} ${time} => `;
-};
-
-
-const merge = ({ original, accumulated = {}, change = {} }) =>
-  ({ original, accumulated: Object.assign(accumulated, change) });
-
-const resumir = e => ({ n: e.summary, d: datetime(e.start.dateTime || e.start.date), l: (e.location || 'Somewhere') });
-
 authorize(auth => {
 
-  console.log(CALENDAR_ID);
+  calendar.events.list({ auth, calendarId: CALENDAR_ID }, (error, response) => {
 
-  calendar.events.list({ auth, calendarId: CALENDAR_ID }, (e, r) => {
-
-    if (e) {
-      return console.log(e);
+    if (error) {
+      return console.log(error);
     }
 
-    console.log(r);
-
-    // r.items
-    //   .map( item => merge({ original: item, change: { name: item.summary } }) )
-    //   .map( wrapped => merge(Object.assign(wrapped, { change: { description: wrapped.original.description } })))
-    //   .map( wrapped => wrapped.accumulated )
-    //   .forEach(i => console.log(i.name));
+    console.log(`Calendar name: ${response.summary}`);
+    console.log(`Last update: ${response.updated}`);
 
   });
 });
